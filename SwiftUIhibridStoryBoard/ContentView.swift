180610d8c0f4439b05c7410aa5a5a0a80da4d567
Shen Jun Wu <shenfive@gmail.com>
2022-08-09 17:16:50 +0800

    Initial Commit

---
+++ SwiftUIhibridStoryBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIhibridStoryBoard
+//
+//  Created by 申潤五 on 2022/8/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIhibridStoryBoard/SwiftUIhibridStoryBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIhibridStoryBoardApp.swift
+//  SwiftUIhibridStoryBoard
+//
+//  Created by 申潤五 on 2022/8/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIhibridStoryBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
